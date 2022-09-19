7bf293fc0253450e1c9faa8f2889d2586f37bdfc
saif-deen <saifgeology7@gmail.com>
2022-09-19 23:18:59 +0200

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+while True:
+    x = int(input("First Number:"))
+    y = input("Sample")
+    z = int(input("Second Number:"))
+    if y=="+":
+        print(x+z)
+    elif y=="_":
+        print(x-z)
+    elif y=="*":
+        print(x*z)
+    else:
+        print(x/z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
